17022f23900aec7b5ab3da8ffd33ccd7dd135e4e
Keith Williams <kawillia@vt.edu>
2012-10-11 20:59:42 -0400

    First pass at Grid class and unit tests.

---
+++ Core/Grid.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameOfLife.Core
+{
+    public class Grid
+    {
+        private Boolean[,] cells;
+
+        public Grid(Int32 numberOfRows, Int32 numberOfColumns)
+        {
+            cells = new Boolean[numberOfRows, numberOfColumns];
+
+            for (var i = 0; i < numberOfRows; i++)
+                for (var j = 0; j < numberOfColumns; j++)
+                    cells[i, j] = false;
+        }
+
+        public void BringToLife(Int32 rowNumber, Int32 columnNumber)
+        {
+            cells[rowNumber - 1, columnNumber - 1] = true;
+        }
+
+        public void Update()
+        {
+            var copiedCells = new Boolean[cells.GetLength(0), cells.GetLength(1)];
+            Array.Copy(cells, copiedCells, cells.Length);
+
+            for (var i = 0; i < cells.GetLength(0); i++)
+            {
+                for (var j = 0; j < cells.GetLength(1); j++)
+                {
+                    var neighbors = GetNeighbors(i, j);
+                    var numberOfLiveNeighbors = neighbors.Count(n => n);
+
+                    if (cells[i, j])
+                        copiedCells[i, j] = numberOfLiveNeighbors > 1 && numberOfLiveNeighbors < 4;
+                    else
+                        copiedCells[i, j] = numberOfLiveNeighbors == 3;
+                }
+            }
+
+            Array.Copy(copiedCells, cells, copiedCells.Length);
+        }
+
+        private IEnumerable<Boolean> GetNeighbors(Int32 rowIndex, Int32 columnIndex)
+        {
+            var neighbors = new List<Boolean>();
+            var numberOfRows = cells.GetLength(0);
+            var numberOfColumns = cells.GetLength(1);
+
+            //for (var i = -1; i <= 1; i++)
+            //{
+            //    for (var j = -1; j <= 1; j++)
+            //    {
+            //        var neighborRowIndex = rowIndex + i;
+            //        var neighborColumnIndex = columnIndex + j;
+
+            //        if (neighborRowIndex != rowIndex && neighborColumnIndex != columnIndex)
+            //            if (AreValidNeighborIndices(neighborRowIndex, neighborColumnIndex))
+            //                neighbors.Add(cells[neighborRowIndex, neighborColumnIndex]);
+            //    }
+            //}
+
+            // Above
+            if (rowIndex > 0)
+                neighbors.Add(cells[rowIndex - 1, columnIndex]);
+
+            // Top Right
+            if (rowIndex > 0 && columnIndex < numberOfColumns - 1)
+                neighbors.Add(cells[rowIndex - 1, columnIndex + 1]);
+
+            // Right
+            if (columnIndex < numberOfColumns - 1)
+                neighbors.Add(cells[rowIndex, columnIndex + 1]);
+
+            // Bottom Right
+            if (rowIndex < numberOfRows - 1 && columnIndex < numberOfColumns - 1)
+                neighbors.Add(cells[rowIndex + 1, columnIndex + 1]);
+
+            // Bottom
+            if (rowIndex < numberOfRows - 1)
+                neighbors.Add(cells[rowIndex + 1, columnIndex]);
+
+            // Bottom Left
+            if (rowIndex < numberOfRows - 1 && columnIndex > 0)
+                neighbors.Add(cells[rowIndex + 1, columnIndex - 1]);
+
+            // Left
+            if (columnIndex > 0)
+                neighbors.Add(cells[rowIndex, columnIndex - 1]);
+
+            // Top Left
+            if (rowIndex > 0 && columnIndex > 0)
+                neighbors.Add(cells[rowIndex - 1, columnIndex - 1]);
+
+            return neighbors;
+        }
+
+        private Boolean AreValidNeighborIndices(Int32 neighborRowIndex, Int32 neighborColumnIndex)
+        {
+            var numberOfRows = cells.GetLength(0);
+            var numberOfColumns = cells.GetLength(1);
+
+            return neighborRowIndex >= 0 &&
+                   neighborRowIndex < numberOfRows &&
+                   neighborColumnIndex >= 0 &&
+                   neighborColumnIndex < numberOfColumns - 1;
+        }
+
+        public Boolean IsCellAlive(Int32 rowNumber, Int32 columnNumber)
+        {
+            return cells[rowNumber - 1, columnNumber - 1];
+        }
+    }
+}
---
+++ UnitTests/GridTests.cs
@@ -0,0 +1,187 @@
+﻿using GameOfLife.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameOfLife.UnitTests
+{
+    [TestClass]
+    public class GridTests
+    {
+        private Grid grid;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            grid = new Grid(4, 4);
+        }
+
+        [TestMethod]
+        public void AnyLiveCellHavingThreeNeighborsWithFewerThanTwoLiveNeighborsDies()
+        {
+            grid.BringToLife(1, 1);
+            grid.Update();
+
+            var isAlive = grid.IsCellAlive(1, 1);
+            Assert.IsFalse(isAlive);
+        }
+
+        [TestMethod]
+        public void AnyLiveCellHavingFiveNeighborsWithFewerThanTwoLiveNeighborsDies()
+        {
+            grid.BringToLife(1, 2);
+            grid.Update();
+
+            var isAlive = grid.IsCellAlive(1, 2);
+            Assert.IsFalse(isAlive);
+        }
+
+        [TestMethod]
+        public void AnyLiveCellHavingEightNeighborsWithFewerThanTwoLiveNeighborsDies()
+        {
+            grid.BringToLife(2, 2);
+            grid.Update();
+
+            var isAlive = grid.IsCellAlive(2, 2);
+            Assert.IsFalse(isAlive);
+        }
+
+        [TestMethod]
+        public void AnyDeadCellHavingThreeNeighborsWithExactlyThreeLiveNeighborsBecomesALiveCell()
+        {
+            grid.BringToLife(1, 2);
+            grid.BringToLife(2, 2);
+            grid.BringToLife(2, 1);
+            grid.Update();
+
+            var isAlive = grid.IsCellAlive(1, 1);
+            Assert.IsTrue(isAlive);
+        }
+
+        [TestMethod]
+        public void AnyDeadCellHavingFiveNeighborsWithExactlyThreeLiveNeighborsBecomesALiveCell()
+        {
+            grid.BringToLife(1, 1);
+            grid.BringToLife(1, 3);
+            grid.BringToLife(2, 3);
+            grid.Update();
+
+            var isAlive = grid.IsCellAlive(1, 2);
+            Assert.IsTrue(isAlive);
+        }
+
+        [TestMethod]
+        public void AnyDeadCellHavingEightNeighborsWithExactlyThreeLiveNeighborsBecomesALiveCell()
+        {
+            grid.BringToLife(1, 2);
+            grid.BringToLife(2, 1);
+            grid.BringToLife(1, 1);
+            grid.Update();
+
+            var isAlive = grid.IsCellAlive(2, 2);
+            Assert.IsTrue(isAlive);
+        }
+        
+        [TestMethod]
+        public void AnyLiveCellHavingFiveNeighborsWithMoreThanThreeLiveNeighborsDies()
+        {
+            grid.BringToLife(1, 2);
+            grid.BringToLife(1, 1);
+            grid.BringToLife(1, 3);
+            grid.BringToLife(2, 3);
+            grid.BringToLife(2, 2);
+            grid.Update();
+
+            var isAlive = grid.IsCellAlive(1, 2);
+            Assert.IsFalse(isAlive);
+        }
+
+        [TestMethod]
+        public void AnyLiveCellHavingEightNeighborsWithMoreThanThreeLiveNeighborsDies()
+        {
+            grid.BringToLife(1, 1);
+            grid.BringToLife(1, 2);
+            grid.BringToLife(1, 3);
+            grid.BringToLife(2, 2);
+            grid.BringToLife(2, 1);
+            
+            grid.Update();
+
+            var isAlive = grid.IsCellAlive(2, 2);
+            Assert.IsFalse(isAlive);
+        }
+
+        [TestMethod]
+        public void AnyLiveCellHavingThreeNeighborsWithExactlyTwoLiveNeighborsLivesOn()
+        {
+            grid.BringToLife(1, 1);
+            grid.BringToLife(1, 2);
+            grid.BringToLife(2, 2);
+            grid.Update();
+
+            var isAlive = grid.IsCellAlive(1, 1);
+            Assert.IsTrue(isAlive);
+        }
+
+        [TestMethod]
+        public void AnyLiveCellHavingFiveNeighborsWithExactlyTwoLiveNeighborsLivesOn()
+        {
+            grid.BringToLife(1, 2);
+            grid.BringToLife(1, 1);
+            grid.BringToLife(2, 3);
+            grid.Update();
+
+            var isAlive = grid.IsCellAlive(1, 2);
+            Assert.IsTrue(isAlive);
+        }
+
+        [TestMethod]
+        public void AnyLiveCellHavingRightNeighborsWithExactlyTwoLiveNeighborsLivesOn()
+        {
+            grid.BringToLife(2, 2);
+            grid.BringToLife(2, 1);
+            grid.BringToLife(1, 1);
+            grid.Update();
+
+            var isAlive = grid.IsCellAlive(2, 2);
+            Assert.IsTrue(isAlive);
+        }
+
+        [TestMethod]
+        public void AnyLiveCellHavingThreeNeighborsWithExactlyThreeLiveNeighborsLivesOn()
+        {
+            grid.BringToLife(1, 1);
+            grid.BringToLife(1, 2);
+            grid.BringToLife(2, 2);
+            grid.BringToLife(2, 1);
+            grid.Update();
+
+            var isAlive = grid.IsCellAlive(1, 1);
+            Assert.IsTrue(isAlive);
+        }
+
+        [TestMethod]
+        public void AnyLiveCellHavingFiveNeighborsWithExactlyThreeLiveNeighborsLivesOn()
+        {
+            grid.BringToLife(1, 2);
+            grid.BringToLife(1, 1);
+            grid.BringToLife(1, 3);
+            grid.BringToLife(2, 3);
+            grid.Update();
+
+            var isAlive = grid.IsCellAlive(1, 2);
+            Assert.IsTrue(isAlive);
+        }
+
+        [TestMethod]
+        public void AnyLiveCellHavingRightNeighborsWithExactlyThreeLiveNeighborsLivesOn()
+        {
+            grid.BringToLife(2, 2);
+            grid.BringToLife(1, 2);
+            grid.BringToLife(2, 1);
+            grid.BringToLife(1, 1);
+            grid.Update();
+
+            var isAlive = grid.IsCellAlive(2, 2);
+            Assert.IsTrue(isAlive);
+        }
+    }
+}
