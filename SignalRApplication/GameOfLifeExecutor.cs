33e17a63c8d27f5b104f1880b6cc9c7f748255cf
Keith Williams <kawillia@vt.edu>
2013-07-29 08:44:38 -0400

    Move the main JavaScript to its own file.

--- SignalRApplication/GameOfLifeExecutor.cs
+++ SignalRApplication/GameOfLifeExecutor.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
-using System.Web;
 using GameOfLife.Core;
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
---
+++ SignalRApplication/Scripts/game-of-life.js
@@ -0,0 +1,28 @@
+﻿$(function () {
+    var gameOfLife = $.connection.gameOfLifeHub;
+    gameOfLife.client.updateLivingCells = function (livingCells) {
+        renderLivingCells(livingCells);
+    };
+
+    $.connection.hub.start().done(function () {
+        $("#restart").click(function () {
+            gameOfLife.server.restart();
+        });
+    });
+
+    function renderLivingCells(livingCells) {
+        var canvas = document.getElementById('gridCanvas');
+        var context = canvas.getContext('2d');
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        $.each(livingCells, function (i) {
+            context.beginPath();
+            context.rect(this.X * 10, this.Y * 10, 10, 10);
+            context.fillStyle = 'black';
+            context.fill();
+            context.lineWidth = 1;
+            context.strokeStyle = 'black';
+            context.stroke();
+        });
+    }
+});
